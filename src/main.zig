b0f9b1f2f1b607d28d8a07cf0eb20e2b5f35fa8a
Jacob Hinchliffe <jacob@nullndvoid.xyz>
2025-02-20 04:36:55 +0000

    Read and print memory map from Limine.

--- src/main.zig
+++ src/main.zig
@@ -7,19 +7,33 @@ const term = @import("term/terminal.zig");
 const assert = std.debug.assert;
 
 pub export var base_revision: limine.BaseRevision linksection(".limine_requests") = .{
-    .revision = 2, // TODO: Support revision 3, this is just nabbed from AndreaOrru/zen.
+    .revision = 3,
 };
 
+pub export var memmap_request: limine.MemoryMapRequest linksection(".limine_requests") = .{};
+
 export fn kmain() callconv(.C) noreturn {
-    if (!base_revision.is_supported()) {
+    if (!base_revision.is_supported() or memmap_request.response == null) {
         amd64.hang();
     }
 
     // TODO: Support scalable screen font and use with Iosevka because it is lush.
     _ = term.Terminal.initialise();
     term.clear(0x082222);
-    term.print("Hello this is a test message!", .{});
+    term.print("Reading memory map from Limine...\n", .{});
 
+    readMemMap();
     gdt.initialise();
     amd64.hang();
+}
+
+fn readMemMap() void {
+    var memmap = memmap_request.response.?;
+    var usable_memory: usize = 0;
+    for (memmap.entries()) |entry| {
+        term.print("{any}\n", .{entry});
+        if (entry.kind == .usable) usable_memory += entry.length;
+    }
+
+    term.print("Got {d} bytes usable memory!\n", .{usable_memory});
 }
